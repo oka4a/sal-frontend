4be175cb55a1aa3216ac900ebeeadf4dce87dbb7
Hossam Okasha <enghossamokashaa@gmail.com>
2024-02-04 08:34:07 +0200

    Add auth routing guard

--- src/router.tsx
+++ src/router.tsx
@@ -3,11 +3,20 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
+import AuthGuard from "./components/guards/AuthGuard";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/login" element={<Login />} />)
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<AuthGuard />}>
+        <Route index element={<Auth />} />
+      </Route>
+      {/* <Route path="/login" element={<Login />} /> */}
+    </>
+  )
 );
-
 
 export default router;
