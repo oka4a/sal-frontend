a973e4ab12bc051e451f2339fa5e32304a27b7da
Hossam Okasha <enghossamokashaa@gmail.com>
2024-02-04 08:31:51 +0200

    Add user and login types

--- src/types/backend.d.ts
+++ src/types/backend.d.ts
@@ -2,3 +2,21 @@ interface LoginData {
   username: string;
   password: string;
 }
+
+interface LoginResponse {
+  token: string;
+}
+
+interface User {
+  avatar: string;
+  bio?: string;
+  created_at: string;
+  email: string;
+  first_name: string;
+  full_name: string;
+  id: number;
+  job: string;
+  last_name: string;
+  phone: string;
+  username: string;
+}
